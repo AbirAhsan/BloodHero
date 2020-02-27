81c93fce7636c97ed761909e744f2c5dd3d8e7b8
Md Abir Ahsan Tahmim <abirahsan122@gmail.com>
2020-02-27 11:43:37 +0600

    Edit login page(pixel overloading problem solved)

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -2,113 +2,106 @@ import 'package:bloodhero/pages/home.dart';
 import 'package:bloodhero/widgets/exitpop.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
           Image(
-            image: AssetImage("assets/donor_poster.png"),
-            fit: BoxFit.cover,
+            image: AssetImage("./assets/tree.jpg"),
             color: Colors.black87,
-            colorBlendMode: BlendMode.darken,
+            colorBlendMode: BlendMode.softLight,
+            fit: BoxFit.fill,
           ),
-          ListView(children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          Container(
+            child: ListView(
               children: <Widget>[
-                Padding(padding: EdgeInsets.only(top: 80.0)),
-                CircleAvatar(
-                  backgroundColor: Colors.green,
-                  radius: 50.0,
-                  child: Icon(
-                    Icons.local_hospital,
-                    color: Colors.red,
-                    size: 80.0,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    FocusScope.of(context).requestFocus(FocusNode());
-                  },
-                  child: Form(
-                    child: Theme(
-                      data: ThemeData(
-                          brightness: Brightness.dark,
-                          primarySwatch: Colors.teal,
-                          inputDecorationTheme: InputDecorationTheme(
-                            labelStyle: TextStyle(
-                                color: Colors.teal,
-                                fontSize: 30.0,
-                                fontFamily: "AlexBrush"),
-                          )),
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            top: 10.0, right: 20.0, left: 20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            TextFormField(
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 12.0),
-                              decoration: InputDecoration(
-                                labelText: "Enter Your Email",
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 10.0),
-                            ),
-                            TextFormField(
-                              style: TextStyle(color: Colors.white),
-                              decoration: InputDecoration(
-                                labelText: "Enter Your password",
-                              ),
-                              keyboardType: TextInputType.text,
-                              obscureText: true,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 10.0),
-                            ),
-                            MaterialButton(
-                              height: 40.0,
-                              minWidth: 150.0,
-                              color: Colors.teal,
-                              textColor: Colors.white,
-                              child: Text(
-                                "Login",
-                                style: TextStyle(fontSize: 20.0),
-                              ),
-                              shape: StadiumBorder(),
-                              splashColor: Colors.orange,
-                              onPressed: () => {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => ExitPopUp(
-                                      HomePage(),
-                                    ),
-                                  ),
-                                ),
-                              },
-                            ),
-                          ],
+                Stack(
+                  fit: StackFit.loose,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * (.5),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 50.0,
+                          child: Icon(
+                            Icons.local_hospital,
+                            color: Colors.red,
+                            size: 80.0,
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * (.3)),
+                        Theme(
+                          data: ThemeData(
+                              brightness: Brightness.dark,
+                              primarySwatch: Colors.teal,
+                              inputDecorationTheme: InputDecorationTheme(
+                                  labelStyle:
+                                      TextStyle(color: Colors.tealAccent))),
+                          child: Padding(
+                            padding: EdgeInsets.all(30.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                FocusScope.of(context)
+                                    .requestFocus(FocusScopeNode());
+                              },
+                              child: Form(
+                                child: Column(
+                                  children: <Widget>[
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        labelText: "Enter Your Email",
+                                      ),
+                                      keyboardType: TextInputType.emailAddress,
+                                    ),
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                          labelText: "Enter Your password"),
+                                      keyboardType:
+                                          TextInputType.visiblePassword,
+                                    ),
+                                    SizedBox(
+                                      height: 30.0,
+                                    ),
+                                    MaterialButton(
+                                        height: 50.0,
+                                        minWidth: 140.0,
+                                        color: Colors.teal,
+                                        textColor: Colors.white,
+                                        child: Text("Login"),
+                                        shape: StadiumBorder(),
+                                        splashColor: Colors.orange,
+                                        onPressed: () => {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          ExitPopUp(
+                                                              HomePage())))
+                                            }),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
-          ])
+          ),
         ],
       ),
     );
